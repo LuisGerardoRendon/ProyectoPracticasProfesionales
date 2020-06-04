39c0e99e4156e224e2ae48401e3272d77e1527cd
rendo <rendo@DESKTOP-0FGPCGK>
2020-06-03 20:37:13 -0500

    ultimo

---
+++ ProyectoConstruccion/src/Controlador/Practicante/ReporteCargado_Controller.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.Practicante;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class ReporteCargado_Controller implements Initializable {
+
+    @FXML
+    private Button botonCancelar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+    
+}
